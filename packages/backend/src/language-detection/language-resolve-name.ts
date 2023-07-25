e87a9e6eefacb7e4e5d99071d56f78614e9504e3
Itay <itay@ox.security>
2023-07-25 17:43:41 +0300

    Rename name map file

--- packages/backend/src/language-detection/language-resolve-name.ts
+++ packages/backend/src/language-detection/language-resolve-name.ts
@@ -1,4 +1,4 @@
-import languagesMap from "./languages-map.json";
+import languagesMap from "./languages-id-to-name-map.json";
 
 export const resolveName = (languageId: string): string | undefined => {
   if (languagesMap[languageId]) {
