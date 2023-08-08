24ffa738abb3515db66dff8b3d3d0ff288ac0bbe
Itay <itay@ox.security>
2023-08-08 20:11:02 +0300

    Add all-languages api

--- packages/backend/src/language-detection/language-resolve-name.ts
+++
@@ -1,9 +0,0 @@
-import languagesMap from "./languages-id-to-name-map.json";
-
-export const resolveName = (languageId: string): string | undefined => {
-  if (languagesMap[languageId]) {
-    return languagesMap[languageId];
-  }
-
-  return undefined;
-};
